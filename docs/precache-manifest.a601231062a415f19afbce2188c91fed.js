2b4b48df4361b4e328ddb2c761008b4e39984b17
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-10-20 15:39:48 +0200

    Create docs

---
+++ docs/precache-manifest.a601231062a415f19afbce2188c91fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59eedfd3cdf550a44eb39b9b83ab663",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87cf8076a2d77a6784a6",
+    "url": "./static/css/main.57d38ca2.chunk.css"
+  },
+  {
+    "revision": "645bc36d5a56aad2391b",
+    "url": "./static/js/2.a7bd0160.chunk.js"
+  },
+  {
+    "revision": "87cf8076a2d77a6784a6",
+    "url": "./static/js/main.cadff31e.chunk.js"
+  },
+  {
+    "revision": "a1097dffc05b5c32af07",
+    "url": "./static/js/runtime-main.30e23a1c.js"
+  },
+  {
+    "revision": "398248efcd4d367398bcc227fdf707a1",
+    "url": "./static/media/pokemon-logo.398248ef.png"
+  }
+]);
