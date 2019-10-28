662e2c1ed943606e1d5e1b2638aa2d46811bd5c3
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-10-29 00:02:32 +0100

    Configure linter

--- README.md
+++ README.md
@@ -7,21 +7,27 @@
 This is a simple web app created for the online training in Adalab's bootcamp. It's developed by [Alexandra Jara](https://github.com/alexandrajaramz), using React.
 
 It's a list of Pokemons (like [pokedex.org](http://pokedex.org)), and the requested features were:
-- List of the 25 first Pokemon using [pokeapi.co](https://pokeapi.co/). 
+- List of the 25 first Pokemon using [pokeapi.co](https://pokeapi.co/).
 - Show the image, name, number and type or types of each Pokemon (double fetch).
 - Filter Pokemons by name.
 
-### ⚙️ How to use 
+### ⚙️ How to use
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 You need to install Node-sass `npm install --save node-sass` and PropTypes `npm install --save prop-types` to work with this project.
 
-Then, you can run `npm start` to use the app in the development mode. 
+Then, you can run `npm start` to use the app in the development mode.
 
 Open [https://localhost:3000](https://localhost:3000) to view it in the browser. The page will reload if you make edits.
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+### Development
+
+This project uses [eslint](https://eslint.org/) to ensure the code follows the defined codestyle. To do so, you just need to execute
+`npm run lint`.
+In case you find errors after the execution, you could give a try to `npm run fixLint` to try to fix found errors :).
 
 ### ⚡️ Have fun!
 <img src="./src/images/pikachu.gif" alt="Happy Pikachu :D" width="300px">
--- src/service/fetch.js
+++ src/service/fetch.js
@@ -1,44 +1,44 @@
 const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=25';
 
-export const findAllPokemons = (notifyLoadedPokemon) => {
-  return fetch(ENDPOINT)
-  .then(response => response.json())
-  .then(data => {
-    for (let item of data.results) {
-      fetch(item.url)
-        .then(response => response.json())
-        .then(result => {
-          const pokemon = parsePokemon(result);
-          notifyLoadedPokemon(pokemon);
-        })
-    }
-  })
-};
-
 function parsePokemon(result) {
-  const types = [];
-  for (let item of result.types) {
-    types.push(item.type.name);
-  }
-  const abilities = [];
-  for (let item of result.abilities) {
-    abilities.push(item.ability.name);
-  }
+  const types = result.types.map(item => item.type.name);
 
-  //aquí queria hacer el otro fetch para meter las evoluciones directamente en el objeto pokemon que paso al state y pasarlo todo por props, accediendo a species.url, pero dice que species no es iterable, al loguear name y url mete una letra por línea (asincronía?)
-  for (let item of result.species.url) {
-    console.log(item);
-  }
+  const abilities = result.abilities.map(item => item.ability.name);
+
+  // aquí queria hacer el otro fetch para meter las evoluciones
+  // directamente en el objeto pokemon que paso al state y pasarlo
+  // todo por props, accediendo a species.url, pero dice que species
+  // no es iterable, al loguear name y url mete una letra por línea
+  // (asincronía?)
+  // for (const item of result.species.url) {
+  //   console.log(item);
+  // }
 
   const pokemon = {
     image: result.sprites.front_default,
     imageBack: result.sprites.back_default,
     name: result.name,
     id: result.id,
-    types: types,
+    types,
     height: result.height,
     weight: result.weight,
-    abilities: abilities,
-  }
+    abilities,
+  };
   return pokemon;
 }
+
+const findDetailForSinglePokemon = (item, notify) => fetch(item.url)
+  .then(response => response.json())
+  .then((result) => {
+    const pokemon = parsePokemon(result);
+    notify(pokemon);
+    return pokemon;
+  });
+
+const findAllPokemons = notifyLoadedPokemon => fetch(ENDPOINT)
+  .then(response => response.json())
+  .then(data => Promise.all(data.results.map(
+    item => findDetailForSinglePokemon(item, notifyLoadedPokemon),
+  )));
+
+export default findAllPokemons;
