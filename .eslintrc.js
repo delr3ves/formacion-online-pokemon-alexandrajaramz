24f490e62c9c8145a6580e8edaf1074fe3cd7687
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-11-01 13:50:24 +0100

    Configure eslint to work fine with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    "jest/globals": true
   },
   extends: [
     'airbnb'
@@ -18,7 +19,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'jest'
   ],
   rules: {
   }
